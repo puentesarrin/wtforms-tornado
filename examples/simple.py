aab87349695eddea0d0729fae2e2a23a1926f8f3
Jorge Puente Sarrín <puentesarrin@gmail.com>
2013-09-25 22:14:16 +0000

    Added simple example.

---
+++ examples/simple.py
@@ -0,0 +1,36 @@
+import os
+import sys
+my_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.abspath(os.path.join(my_dir, '..')))
+
+import tornado.ioloop
+import tornado.web
+
+from wtforms.fields import IntegerField
+from wtforms.validators import Required
+from wtforms_tornado import Form
+
+class SumForm(Form):
+
+    a = IntegerField(validators=[Required()])
+    b = IntegerField(validators=[Required()])
+
+class SumHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+    def post(self):
+        form = SumForm(self.request.arguments)
+        if form.validate():
+            self.write("Ok!")
+        else:
+            self.set_status(400)
+            self.write(form.errors)
+
+application = tornado.web.Application([
+    (r"/", SumHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
