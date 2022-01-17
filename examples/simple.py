c69d178418c5c7b998077fe303f8a599b7052d17
Jorge Puente-Sarrín <puentesarrin@gmail.com>
2022-01-17 01:33:17 -0500

    Fixed script example.

--- examples/simple.py
+++ examples/simple.py
@@ -7,13 +7,13 @@ import tornado.ioloop
 import tornado.web
 
 from wtforms.fields import IntegerField
-from wtforms.validators import Required
+from wtforms.validators import DataRequired
 from wtforms_tornado import Form
 
 class SumForm(Form):
 
-    a = IntegerField(validators=[Required()])
-    b = IntegerField(validators=[Required()])
+    a = IntegerField(validators=[DataRequired()])
+    b = IntegerField(validators=[DataRequired()])
 
 class SumHandler(tornado.web.RequestHandler):
     def get(self):
